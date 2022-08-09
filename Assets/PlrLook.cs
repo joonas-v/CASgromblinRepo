4ac52e36a8473b2161a3fadeef0e60a79423e092
joonas-v <joonas.vosu@gmail.com>
2022-08-09 19:34:32 +0300

    basic player
    added basic player character, can only move and jump for now

---
+++ Assets/PlrLook.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlrLook : MonoBehaviour
+{
+    public Transform player;
+
+    public float mouseSensitivity = 100f;
+
+    private float x, y = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //input
+        x += Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        y += Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+
+        //clamp
+        x = Mathf.Clamp(x, -90, 90);
+
+        //rotation
+        transform.localRotation = Quaternion.Euler(-x, 0, 0);
+        player.transform.localRotation = Quaternion.Euler(0, y, 0);
+
+        //cursor locking/unlocking
+        if (Input.GetKeyDown(KeyCode.Escape) && Cursor.lockState == CursorLockMode.Locked)
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && Cursor.lockState == CursorLockMode.None)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+}
---
+++ Assets/PlrMove.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlrMove : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public float speed = 5f;
+    public float jumpForce = 10f;
+    public LayerMask lmask;
+    public bool isGrounded;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //input
+        float x = Input.GetAxisRaw("Horizontal") * speed;
+        float y = Input.GetAxisRaw("Vertical") * speed;
+
+
+        //isground
+        isGrounded = Physics.CheckSphere(new Vector3(transform.position.x, transform.position.y - 1, transform.position.z), 0.4f, lmask);
+
+        //jump
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+        }
+
+        //movement
+        Vector3 movePos = transform.right * x + transform.forward * y;
+        Vector3 newMovePos = new Vector3(movePos.x, rb.velocity.y, movePos.z);
+        rb.velocity = newMovePos;
+        
+    }
+
+
+}
